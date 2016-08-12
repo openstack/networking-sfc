39b3b2690c0891fc88ae797a7fd925356f0ef568
Akihiro Motoki <amotoki@gmail.com>
2016-08-13 00:10:59 +0900

    Use constraints environment for testing
    In addition, as per [1], we can now use constraints targets in post queues,
    so there is no need to override install_commands in api-ref target.
    
    [1] http://lists.openstack.org/pipermail/openstack-dev/2016-August/101474.html
    
    Change-Id: Ica1c9588baf3c78bb27d6b298a138beae47980b0
    Signed-off-by: Akihiro Motoki <amotoki@gmail.com>

--- tools/tox_install.sh
+++ tools/tox_install.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 # Many of neutron's repos suffer from the problem of depending on neutron,
 # but it not existing on pypi.
@@ -13,10 +13,40 @@
 # default:
 # pip install {opts} {packages}
 
+set -x
+
+ZUUL_CLONER=/usr/zuul-env/bin/zuul-cloner
+neutron_installed=$(echo "import neutron" | python 2>/dev/null ; echo $?)
+NEUTRON_DIR=$HOME/neutron
+BRANCH_NAME=stable/mitaka
+
 set -e
 
-echo "PIP HARDCODE" > /tmp/tox_install.txt
-pip install -U -egit+https://git.openstack.org/openstack/neutron#egg=neutron
+install_cmd="pip install -c$1"
+shift
 
-pip install -U $*
+if [ -d "$NEUTRON_DIR" ]; then
+    echo "FOUND Neutron code at $NEUTRON_DIR - using"
+    $install_cmd -U -e $NEUTRON_DIR
+elif [ $neutron_installed -eq 0 ]; then
+    location=$(python -c "import neutron; print(neutron.__file__)")
+    echo "ALREADY INSTALLED at $location"
+elif [ -x "$ZUUL_CLONER" ]; then
+    echo "USING ZUUL CLONER to obtain Neutron code"
+    cwd=$(/bin/pwd)
+    cd /tmp
+    $ZUUL_CLONER --cache-dir \
+        /opt/git \
+        --branch $BRANCH_NAME \
+        git://git.openstack.org \
+        openstack/neutron
+    cd openstack/neutron
+    $install_cmd -e .
+    cd "$cwd"
+else
+    echo "LOCAL - Obtaining Neutron code from git.openstack.org"
+    $install_cmd -U -egit+https://git.openstack.org/openstack/neutron@$BRANCH_NAME#egg=neutron
+fi
+
+$install_cmd -U $*
 exit $?
