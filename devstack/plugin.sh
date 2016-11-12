7c06ec1d691f3a52a8f8810cfa5e66be2116ce2c
gong yong sheng <gong.yongsheng@99cloud.net>
2016-11-12 13:46:23 +0000

    Allow SFC installation with q-agt service only
    In q-agt service node without q-svc service, the DB operation
    and some configurations are not needed.
    
    Change-Id: I432e1a602470f14c25d443912bf0ea034d33ceea
    Closes-bug: 1641267

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -5,20 +5,31 @@ function networking_sfc_install {
     setup_develop $NETWORKING_SFC_DIR
 }
 
-function networking_sfc_configure_common {
+function _networking_sfc_install_server {
     _neutron_service_plugin_class_add $NEUTRON_FLOWCLASSIFIER_PLUGIN
     _neutron_service_plugin_class_add $NEUTRON_SFC_PLUGIN
-    iniset /$Q_PLUGIN_CONF_FILE agent extensions "sfc"
     iniset $NEUTRON_CONF DEFAULT service_plugins $Q_SERVICE_PLUGIN_CLASSES
     iniadd $NEUTRON_CONF sfc drivers $NEUTRON_SFC_DRIVERS
     iniadd $NEUTRON_CONF flowclassifier drivers $NEUTRON_FLOWCLASSIFIER_DRIVERS
-    _neutron_deploy_rootwrap_filters $NETWORKING_SFC_DIR
     neutron-db-manage --config-file $NEUTRON_CONF --config-file /$Q_PLUGIN_CONF_FILE --subproject networking-sfc upgrade head
+}
+
+function _networking_sfc_install_agent {
+    iniset /$Q_PLUGIN_CONF_FILE agent extensions "sfc"
+    _neutron_deploy_rootwrap_filters $NETWORKING_SFC_DIR
+}
+
+function networking_sfc_configure_common {
+    if is_service_enabled q-svc; then
+        _networking_sfc_install_server
+    elif is_service_enabled q-agt; then
+        _networking_sfc_install_agent
+    fi
 }
 
 
 if [[ "$1" == "stack" && "$2" == "install" ]]; then
-    if [[ "$SFC_UPDATE_OVS" == "True" ]]; then
+    if is_service_enabled q-agt && [[ "$SFC_UPDATE_OVS" == "True" ]]; then
         source $NETWORKING_SFC_DIR/devstack/lib/ovs
         # The OVS_BRANCH variable is used by git checkout.
         OVS_BRANCH=v2.4.0
