8d32926ac78c7d4959873545dd0f0170bd493aaf
venkatamahesh <Venkata.Kotha@infinite.com>
2017-01-21 13:57:38 +0530

    Remove unused module index in documentation
    Change-Id: Ia952cea9db530d2a330e60c581c378db59c998fe
    Closes-Bug: #1656126

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -22,7 +22,6 @@ sys.path.insert(0, os.path.abspath('../..'))
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = [
-    'sphinx.ext.autodoc',
     'oslosphinx'
 ]
 
--- doc/source/index.rst
+++ doc/source/index.rst
@@ -61,6 +61,5 @@ Indices and tables
 ------------------
 
 * :ref:`genindex`
-* :ref:`modindex`
 * :ref:`search`
 
