b18b6da02679e0534e7f6612a3d80c4f08645b41
Bernard Cafarelli <bcafarel@redhat.com>
2017-02-23 22:08:23 +0100

    Drop localrc references
    devstack-gate supports local.conf over localrc now:
    https://review.openstack.org/#/c/326585/
    
    This broke our tempest check, so dropping the only element filling
    localrc file (sourcing devstackgaterc is needed all the time anyway to
    get the DEVSTACK_GATE_TEMPEST_REGEX variable)
    
    Change-Id: I9ee5e18cc5c879de500517e9c663ce99d01218d6
    Closes-Bug: 1667485
    (cherry picked from commit f081f642525b6027f1cad114d3d835bfd507e7dc)

--- devstack/post_test_hook.sh
+++ devstack/post_test_hook.sh
@@ -6,17 +6,12 @@ GATE_DEST=$BASE/new
 DEVSTACK_PATH=$GATE_DEST/devstack
 
 source $DEVSTACK_PATH/functions
-source $DEVSTACK_PATH/localrc
 
 TEMPEST_CODE_DIR="$BASE/new/tempest"
-TEMPEST_DATA_DIR="$DATA_DIR/tempest"
+TEMPEST_DATA_DIR="$BASE/data/tempest"
 NETWORKING_SFC_DIR="$BASE/new/networking-sfc"
 
-IS_GATE=$(trueorfalse False IS_GATE)
-if [[ "$IS_GATE" == "True" ]]
-then
-    source $NETWORKING_SFC_DIR/devstack/devstackgaterc
-fi
+source $NETWORKING_SFC_DIR/devstack/devstackgaterc
 
 owner=stack
 sudo_env="TEMPEST_CONFIG_DIR=$TEMPEST_CODE_DIR/etc"
