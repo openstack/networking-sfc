5cb1e424c9c38bfd75dd2f086ca6e21ee111ec42
Armando Migliaccio <armamig@gmail.com>
2017-03-07 16:08:45 -0800

    Be explicit about the extensions being tested
    Otherwise there the danger of skipping those important for SFC [1]
    
    Co-Authored-By: Ihar Hrachyshka <ihrachys@redhat.com>
    Co-Authored-By: Sean Dague <sean@dague.net>
    Original-Commit: Ibd0f67f9131e7f67f3a4a62cb6ad28bf80e11bbf
    
    Needed-By: Ie995f6875a1e2b4c1e47a6596d7e84d9d72c768b
    [1] http://logs.openstack.org/11/442111/2/check/gate-tempest-dsvm-networking-sfc-ubuntu-xenial/0d87aca/console.html#_2017-03-07_20_40_34_903985
    
    Change-Id: I4a51e25b7fc3c39beea69433e3230838ae2f65bc

--- networking_sfc/tests/contrib/gate_hook.sh
+++ networking_sfc/tests/contrib/gate_hook.sh
@@ -9,17 +9,34 @@ NEUTRON_PATH=$GATE_DEST/neutron
 NETWORKING_SFC_PATH=$GATE_DEST/networking-sfc
 GATE_HOOKS=$NETWORKING_SFC_PATH/networking_sfc/tests/contrib/hooks
 DEVSTACK_PATH=$GATE_DEST/devstack
-LOCAL_CONF=$DEVSTACK_PATH/local.conf
+LOCAL_CONF=$DEVSTACK_PATH/late-local.conf
+DSCONF=/tmp/devstack-tools/bin/dsconf
+
+# Install devstack-tools used to produce local.conf; we can't rely on
+# test-requirements.txt because the gate hook is triggered before neutron is
+# installed
+sudo -H pip install virtualenv
+virtualenv /tmp/devstack-tools
+/tmp/devstack-tools/bin/pip install -U devstack-tools==0.4.0
+
+# Inject config from hook into localrc
+function load_rc_hook {
+    local hook="$1"
+    local tmpfile
+    local config
+    tmpfile=$(tempfile)
+    config=$(cat $GATE_HOOKS/$hook)
+    echo "[[local|localrc]]" > $tmpfile
+    $DSCONF setlc_raw $tmpfile "$config"
+    $DSCONF merge_lc $LOCAL_CONF $tmpfile
+    rm -f $tmpfile
+}
 
 
-# Inject config from hook
+# Inject config from hook into local.conf
 function load_conf_hook {
     local hook="$1"
-    config=$(cat $GATE_HOOKS/$hook)
-    export DEVSTACK_LOCAL_CONFIG+="
-# generated from hook '$hook'
-${config}
-"
+    $DSCONF merge_lc $LOCAL_CONF $GATE_HOOKS/$hook
 }
 
 
@@ -30,6 +47,7 @@ case $VENV in
     GATE_STACK_USER=stack
     PROJECT_NAME=networking-sfc
     IS_GATE=True
+    LOCAL_CONF=$DEVSTACK_PATH/local.conf
 
     source $DEVSTACK_PATH/functions
 
@@ -43,7 +61,11 @@ case $VENV in
     # Make the workspace owned by the stack user
     sudo chown -R $STACK_USER:$STACK_USER $BASE
     ;;
-
+"dsvm-networking-sfc")
+    load_rc_hook api_extensions
+    export DEVSTACK_LOCALCONF=$(cat $LOCAL_CONF)
+    $BASE/new/devstack-gate/devstack-vm-gate.sh
+    ;;
 *)
     echo "Unrecognized environment $VENV".
     exit 1
