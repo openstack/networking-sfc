f211d5d2ba2bfb482de84b1884df6e846277b952
Bernard Cafarelli <bcafarel@redhat.com>
2021-07-07 15:52:05 +0200

    [functional] Remove duplicate code from parent test class
    The server start functional test had code identical to parent class
    TestPluginWorker that can be dropped. It also broke in networking-sfc
    after neutron https://review.opendev.org/c/openstack/neutron/+/793899
    was merged
    
    Change-Id: I2e31be14f7b768b563d1c6f4babb08ec6c7ca9a2
    Closes-Bug: #1934900

--- networking_sfc/tests/functional/test_service.py
+++ networking_sfc/tests/functional/test_service.py
@@ -12,14 +12,8 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-import os
-import os.path
-import signal
-
-
 from oslo_config import cfg
 
-from neutron.common import utils
 from neutron.tests.functional import test_server
 
 from networking_sfc.services.flowclassifier.common import (  # noqa
@@ -29,36 +23,6 @@ from networking_sfc.services.sfc.common import (  # noqa
 
 
 class TestService(test_server.TestPluginWorker):
-    def _fake_start(self):
-        with open(self.temp_file, 'ab') as f:
-            f.write(test_server.FAKE_START_MSG)
-
-    def _fake_reset(self):
-        with open(self.temp_file, 'ab') as f:
-            f.write(test_server.FAKE_RESET_MSG)
-
-    def _test_restart_service_on_sighup(self, service, workers=1):
-        self._start_server(callback=service, workers=workers)
-        os.kill(self.service_pid, signal.SIGHUP)
-        expected_msg = (
-            test_server.FAKE_START_MSG * workers +
-            test_server.FAKE_RESET_MSG * (workers + 1))
-        expected_size = len(expected_msg)
-
-        utils.wait_until_true(
-            lambda: (os.path.isfile(self.temp_file) and
-                     os.stat(self.temp_file).st_size ==
-                     expected_size),
-            timeout=5, sleep=0.1,
-            exception=RuntimeError(
-                "Timed out waiting for file %(filename)s to be created and "
-                "its size become equal to %(size)s." %
-                {'filename': self.temp_file,
-                 'size': expected_size}))
-        with open(self.temp_file, 'rb') as f:
-            res = f.readline()
-            self.assertEqual(expected_msg, res)
-
     def test_start(self):
         cfg.CONF.set_override(
             'service_plugins', [
